484fcd60e813eab92f25f5aa1fddd0809068f261
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2024-11-26 12:38:49 -0300

    Create Apresentação.py

---
+++ Apresentação.py
@@ -0,0 +1,7 @@
+Nome = input("Digite o Seu Nome");
+Idade = input("Informe a Sua Idade");
+Cidade = input("Informe Sua Cidade"); 
+
+Apresentar = "Olá!" + "Seja Bem Vindo" + "Meu Nome é {Nome}. Eu Tenho {Idade} anos e moro em {Cidade}."; """String Exibe em conjunto os atributos"""
+
+print(Apresentar + "Seja Bem Vindo!")
