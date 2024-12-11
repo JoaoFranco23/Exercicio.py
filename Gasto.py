ebffb4bdd374a8073dd46a2fd4b7a985369f89b6
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2024-12-11 02:26:59 -0300

    Update Gasto.py

--- Gasto.py
+++ Gasto.py
@@ -41,15 +41,19 @@ def menu():
                     print(f"\nResultado {Nome}:") 
                     print(f"Soma Acumulado: {soma}" + f"Divisão: {div}" + f"Multiplicado: {mult}")
                     
+                    def opção(Sair):
+                        if sair == 0:
+                            Sair = input("Digite para Sair (0 = Sair)")
+                        return Sair
                     while True:
                         try:
-                            Sair = input("Para Sair Digite (0 = Sair)") 
-                            if Sair in menu:
-                                print("Saindo... Até Mais!")
-                                break
-                            elif Sair in menu:
-                                print("Saindo da Opção {Sair}")
+                           menu()
+                           if sair == "0":
+                               print("Saindo... Até mais!")
+                               break
                             else:
-                                print("Opção inválida! Tente novamente.")
+                                print("Opção inválida! Tente Novamente.")
+                            except Exception(e):
+                                print(f"Ocorreu um erro: {e}") 
+                                menu()
                                 
-                            
