57f76ffba1ec57a5045f3b4fb91d1c1fd1516d43
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2025-01-28 22:22:48 -0300

    Update CalcSimples.py

--- CalcSimples.py
+++ CalcSimples.py
@@ -1,11 +1,23 @@
 Num1 = float(input("Insira um Numero: "))
 Num2 = float(input("Insira o Segundo Numero: "))
-operação = type(str("+", "-", "*", "/"))
 
-def operação():
+soma = Num1 + Num2
+Subtração = Num1 - Num2
+multiplicação = Num1 * Num2
+Divisao = Num1 / Num2
+
+def operaçao():
     if soma == "+":
         return Num1 + Num2
-    elif subtração == "-":
+    elif Subtração == "-":
         return Num1 - Num2
+    elif multiplicação == "*":
+        return Num1 * Num2
+    elif Divisao == "/":
+        if Num2 != 0:
+            return Num1 / Num2
+        else:
+            pass
 
-resultado = f"A Soma do {Num1} e {Num2} é {Num1 + Num2} , A Subtração de {Num1 - Num2} é, A multiplicação de {Num1 * Num2} é"
+resultado = f"A Soma do {Num1} e {Num2} é {Num1 + Num2} , A Subtração de {Num1 - Num2} é, A multiplicação de {Num1 * Num2} é" 
+print(resultado)
