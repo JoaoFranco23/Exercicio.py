b7c09be1bb91abd8ffd7f098106de1c3e0fd40fa
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2025-01-28 22:45:09 -0300

    Create Pandas_Cadastro.py

---
+++ Pandas_Cadastro.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+Informação = []
+
+Dados = ["Nome", "Idade", "Cidade", "Curso"]
+
+def Informação():
+    while True:
+        Nome = input("Informe o Seu Nome")
+        print(f"Digite o Seu Nome {Nome}: ") 
+        if Idade 
+            
+        
