8c0849982a2e8a5e0395bc4ef700b0d5deb816fd
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2025-02-05 22:47:11 -0300

    Update and rename Pandas_Cadastro.py to Exercicio1.py

---
+++ Exercicio1.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. 
+#Interação de uma variavel para inserir um numero 
+Numero = int(input("Informe um Número: "))
+
+#Chamada de uma string, interpolando o uso de uma variavel 
+Num = f"O Número Digitado foi: {Numero}" 
+print(Num)
+
