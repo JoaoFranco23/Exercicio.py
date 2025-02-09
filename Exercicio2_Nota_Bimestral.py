43dba2aa862fc61237e2817a485f06678d887b40
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2025-02-09 17:11:47 -0300

    Update Exercicio2_Nota_Bimestral.py

--- Exercicio2_Nota_Bimestral.py
+++ Exercicio2_Nota_Bimestral.py
@@ -1,5 +1,7 @@
 #Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+#Foi utilizado uma variavel nome para ser inserido
 Nome = input("Informe o seu Nome: ")
+#Variavel das notas bimestrais 
 Nota1 = float(input("Informe a Nota do Primeiro Bimestre: "))
 Nota2 = float(input("Informe a Nota do Segundo Bimestre: "))
 Nota3 = float(input("Informe a Nota do Terceiro Bimestre: "))
@@ -11,7 +13,8 @@ print(Resultado)
 Aluno = f"Olá Aluno {Nome}, A média da Sua Nota é {Resultado}"
 print(Aluno)
 
+#Na função, é declarado se a media for maior que 6, significa que passou, senão reprovado
 if Media > 6:
     print(f"A Média do Aluno {Media} foi {Resultado}. Está Aprovado".format(Nome, Media))
 elif Media < 6:
-    print(f"A Média do Aluno {Media} foi {Resultado}. Ele Foi Reprovado".format(Nome.title(), Media))
+    print(f"A Média do Aluno {Media} foi {Resultado}. Ele Foi Reprovado".format(Nome.title(), Media)) #Dentro dessa tag, possui uma variavel que transforma a string em title
